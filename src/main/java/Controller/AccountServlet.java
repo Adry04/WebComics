b902b7082b4fbb58bf8486fa8351c3f39e85b95c
Adry <adrianodevita8@gmail.com>
2024-06-21 15:53:26 +0200

    User Logout

---
+++ src/main/java/Controller/AccountServlet.java
@@ -0,0 +1,44 @@
+package Controller;
+
+import java.io.*;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.annotation.*;
+
+@WebServlet("/account")
+public class AccountServlet extends HttpServlet {
+
+    public void init() {
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        try {
+            HttpSession session = request.getSession(false);
+            if (session == null || session.getAttribute("userId") == null) {
+                throw new ServletException("You are not logged in");
+            }
+
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/WEB-INF/account.jsp");
+            rd.forward(request, response);
+        } catch (ServletException e) {
+            System.out.println(e);
+            String contextPath = request.getContextPath();
+            response.sendRedirect(contextPath + "/");
+        }
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String action = request.getParameter("action");
+        System.out.println(action);
+        HttpSession session = request.getSession(false);
+        if(action.equals("logout")) {
+            session.invalidate();
+        }
+    }
+
+    public void destroy() {
+    }
+}
---
+++ src/main/webapp/WEB-INF/account.jsp
@@ -0,0 +1,40 @@
+<html>
+<head>
+        <title> <%=session.getAttribute("nome")%> - Account </title>
+</head>
+<body>
+    <%@include file="navbar.jsp"%>
+    <h1>Ciao <%=session.getAttribute("nome")%></h1>
+    <h2>Il mio account</h2>
+    <div class="section-container">
+        <div class="subsection">
+            <!--ORDINI-->
+        </div>
+        <div class="subsection">
+            <!--PAGAMENTI-->
+        </div>
+        <div class="subsection">
+            <!--INDIRIZZI DI SPEDIZIONE-->
+        </div>
+        <button onclick="logout()" class="logout-button">LOGOUT</button>
+    </div>
+    <script type="text/javascript">
+        const logout = () => {
+            const url = document.URL
+            console.log(url)
+            const body = 'action=logout';
+            const response = fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: body //passa il parametro con questo valore
+            }).then(e => {
+                window.location = document.baseURI
+            }).catch(error => {
+                console.log(error)
+            })
+        }
+    </script>
+</body>
+</html>

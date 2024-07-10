daa77402260f2d13111123be76c0d0d7c5367f1b
PellegrinoPiccolo <152791429+PellegrinoPiccolo@users.noreply.github.com>
2024-07-10 21:18:06 +0200

    https filter

---
+++ src/main/java/Controller/Filters/HttpsFilter.java
@@ -0,0 +1,31 @@
+package Controller.Filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "HttpsControlFilter", urlPatterns = "/*")
+public class HttpsFilter extends HttpFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        boolean isSecure = request.isSecure();
+        System.out.println(isSecure);
+        if (!isSecure) { //TODO va messo il negato
+            HttpServletRequest requestServlet = (HttpServletRequest) request;
+            String serverName = requestServlet.getServerName();
+            int serverPort = 8443;
+            String contextPath = requestServlet.getContextPath(); // Aggiunge il contesto dell'applicazione
+            String queryString = requestServlet.getQueryString();
+
+            String redirectUrl = "https://" + serverName + ":" + serverPort + contextPath + "/" +
+                    (queryString == null ? "" : "?" + queryString);
+            System.out.println(redirectUrl);
+            ((HttpServletResponse) response).sendRedirect(redirectUrl);
+            return;
+        }
+        chain.doFilter(request, response);
+    }
+}
